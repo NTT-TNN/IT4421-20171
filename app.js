754b1f4f43a19ee23ebb084c85002f2d0240b037
NTT-TNN <nguyentienthao96@gmail.com>
2017-10-23 15:20:00 +0700

    MVC 2

--- app.js
+++ app.js
@@ -11,38 +11,11 @@ var port= process.env.port | 8000;
 var index = require('./routes/index');
 var DK_BoiBan = require('./routes/DK_BoiBan.js');
 var DK_ThuNgan = require('./routes/DK_ThuNgan.js');
+require('./routes/socket.js')(io);
 
 app.use('/DK_BoiBan', DK_BoiBan);
 app.use('/DK_ThuNgan', DK_ThuNgan);
 
 server.listen(port,function(){
   console.log("App running at port",port);
-});
-
-
-
-
-
-var order = [];
-var banhang = [];
-io.on("connection",function (socket){
-  console.log("co thang nao da vao id cua no la: " + socket.id);
-  socket.on('name' , function(data) {
-    // console.log('name event: ', data, '- id: ', socket.id);
-    if (data.name === 'order') {
-      order.push(socket.id);
-    } else {
-      banhang.push(socket.id);
-    }
-  });
-  socket.on("id_ban",function(data){
-    // console.log(data.cafe +" " + data.suachua);
-    // console.log('to id: ', banhang[0]);
-    io.emit("du_lieu",data);
-  });
-  //...
-  socket.on('disconnect', function() {
-    banhang = [];
-    console.log('socket disconnect');
-  })
 });
