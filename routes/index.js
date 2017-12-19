29c642a444717508fa5bc7a0f3e74afc8e4f986c
NTT-TNN <nguyentienthao96@gmail.com>
2017-12-19 14:13:07 +0700

    co dinh message

--- routes/index.js
+++ routes/index.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   }
 
   res.render('index',{
-    message:req.flash('loginMessage'),
+    message:"req.flash('loginMessage')",
     type:null,
     id:null,
     username:null,
